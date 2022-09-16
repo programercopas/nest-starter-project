4438fe73ddead6faa4f4bb43f5f4b4196a6ad999
Indra Hasan <indra.hasan@sicepat.com>
2022-09-16 14:43:01 +0700

    add scripts for migration

--- src/entities/example.ts
+++ src/entities/example.ts
@@ -1,23 +1,23 @@
 import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
-@Entity('example')
+@Entity('examples')
 class Example extends BaseEntity {
 
-    @PrimaryGeneratedColumn('uuid',{ name: 'id'})
+    @PrimaryGeneratedColumn('uuid', {name: 'id'})
     id: string;
 
-    @Column('varchar',{ name: 'first_name' })
+    @Column('varchar', {name: 'first_name'})
     firstName: string;
 
-    @Column('varchar',{ name: 'last_name' })
+    @Column('varchar', {name: 'last_name'})
     lastName: string;
 
-    @Column('varchar',{ name: 'email' })
+    @Column('varchar', {name: 'email'})
     email: string;
 
-    @Column('varchar',{ name: 'phone' })
+    @Column('varchar', {name: 'phone'})
     phone: string;
 
-    @Column('text',{ name: 'address' })
+    @Column('text', {name: 'address'})
     address: string;
 }

4d1a5733218fb8e791c9994cfa9dd2f35523c6bc
Indra Hasan <indra.hasan@sicepat.com>
2022-09-22 00:02:37 +0700

    add global module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { ExampleModule } from './example/example.module';
+import { GlobalModule } from './global/global.module';
 @Module({
-  imports: [ConfigModule, DatabaseModule, ExampleModule],
+  imports: [ConfigModule, DatabaseModule, ExampleModule, GlobalModule],
   controllers: [],
   providers: [],
 })

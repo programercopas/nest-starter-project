01ce63824c9ed90c8374bcff598e2f8a3fc5ba39
Indra Hasan <indra.hasan@sicepat.com>
2022-09-23 16:58:21 +0700

    remove global model

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { ExampleModule } from './example/example.module';
-import { GlobalModule } from './global/global.module';
 @Module({
-  imports: [ConfigModule, DatabaseModule, ExampleModule, GlobalModule],
+  imports: [ConfigModule, DatabaseModule, ExampleModule],
   controllers: [],
   providers: [],
 })
